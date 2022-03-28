2d8d913a09d9846c2aeb09d9de6168addcbd0001
LUUDUCDAT1108 <82382481+LUUDUCDAT1108@users.noreply.github.com>
2022-03-28 07:20:55 +0700

    first commit

---
+++ LuuDucDat2022958/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LuuDucDat2022958.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
